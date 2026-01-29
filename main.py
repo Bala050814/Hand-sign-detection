4b6ac1ee429569fdad6e38c19c98be07f52f68ec
Christober001 <christobermaria05@gmail.com>
2026-01-29 10:56:52 +0530

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import cv2
+import google.generativeai as genai
+import pyttsx3
+import time
+import mediapipe as mp
+import numpy as np
 
+# ---------------- GEMINI SETUP ----------------
+
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyAhofjCvOsJ9_WDbgkBoLiGVy08Kz1nc_o")
+
+model = genai.GenerativeModel("models/gemini-2.5-flash")
