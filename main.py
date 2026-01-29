96c719f00f8e1b0239c64e8a530168a5028b436f
Balamurugan R <balamurugan.r050814@gmail.com>
2026-01-29 10:58:56 +0530

    Add hand gesture recognition logic

--- main.py
+++ main.py
@@ -12,3 +12,42 @@ import google.generativeai as genai
 genai.configure(api_key="AIzaSyAhofjCvOsJ9_WDbgkBoLiGVy08Kz1nc_o")
 
 model = genai.GenerativeModel("models/gemini-2.5-flash")
+
+# ---------------- VOICE ----------------
+engine = pyttsx3.init()
+
+# ---------------- MEDIAPIPE ----------------
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+
+hand_detector = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=1,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.7
+)
+
+# ---------------- CAMERA ----------------
+cap = cv2.VideoCapture(0)
+
+# ---------------- GESTURE LOGIC ----------------
+def count_fingers(hand_landmarks):
+    tips = [8, 12, 16, 20]
+    count = 0
+    for tip in tips:
+        if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[tip - 2].y:
+            count += 1
+    return count
+
+gesture_map = {
+    0: "Help",
+    1: "Yes",
+    2: "No",
+    3: "I need water",
+    4: "Hello"
+}
+
+last_time = 0
+
+print("Starting hand gesture recognition...")
+print("Press 'ESC' to exit")
